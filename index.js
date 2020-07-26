6ab0ad971931cdedf51f799b4730d2878d10b9bf
Kenneth Nadeau <kennethdev96@gmail.com>
2020-07-26 16:40:56 -0400

    Rename to RequestOtpCode

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+if (__DEV__) {
+  const React = require('react');
+  const whyDidYouRender = require('@welldone-software/why-did-you-render');
+  whyDidYouRender(React, {
+    trackAllPureComponents: true,
+  });
+}
+
 import './src/locale/i18n';
 import './src/navigation';
